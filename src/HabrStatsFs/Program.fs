22b7010898e3df19cecc2e5947230652819a1fd8
Pavel Tupitsyn <ptupitsyn@apache.org>
2019-10-10 20:13:45 +0300

    Implement saveComments

--- src/HabrStatsFs/Program.fs
+++ src/HabrStatsFs/Program.fs
@@ -1,14 +1,17 @@
 ﻿module HabrStatsFs.Program
 
 open CommentLoader
+open IgniteStorage
 
 [<EntryPoint>]
 let main argv =
     let articleId = 470966;
 
-    loadComments articleId
-        |> Seq.sortByDescending (fun c -> c.Score)
-        |> Seq.map (fun c -> sprintf "[%i] %s: %s" (c.Score) (c.Author.Login) (stripTags c.Message))
-        |> Seq.iter (fun c -> printf "\n\n%s" c)
+    loadComments articleId |> saveComments |> ignore
+
+//    loadComments articleId
+//        |> Seq.sortByDescending (fun c -> c.Score)
+//        |> Seq.map (fun c -> sprintf "[%i] %s: %s" (c.Score) (c.Author.Login) (stripTags c.Message))
+//        |> Seq.iter (fun c -> printf "\n\n%s" c)
 
     0

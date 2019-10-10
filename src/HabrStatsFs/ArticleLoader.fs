a47c79f15c00e0e5a4ac115c1b1968a24c3cf835
Pavel Tupitsyn <ptupitsyn@apache.org>
2019-10-10 21:20:52 +0300

    Iterate over all articles from new to old

--- src/HabrStatsFs/ArticleLoader.fs
+++ src/HabrStatsFs/ArticleLoader.fs
@@ -1,3 +1,12 @@
 module HabrStatsFs.ArticleLoader
 
+open HabrStatsFs
+open ServiceTypes
+
 let articlesUrl = "https://m.habr.com/kek/v1/articles/?sort=rating&page=1&fl=ru&hl=ru"
+
+let getMaxArticleId() =
+    (Articles.Load articlesUrl).Data.Articles
+        |> Seq.map (fun a -> a.Id)
+        |> Seq.max
+        |> int64
--- src/HabrStatsFs/Program.fs
+++ src/HabrStatsFs/Program.fs
@@ -1,13 +1,20 @@
 ﻿module HabrStatsFs.Program
 
 open CommentLoader
+open ArticleLoader
+open HabrStatsFs
 open IgniteStorage
 
 [<EntryPoint>]
 let main argv =
-    let articleId = int64 470966;
+    let maxArticleId = getMaxArticleId()
 
-    loadComments articleId |> saveComments
-    articleId |> saveArticle
+    let processArticle (articleId: int64) =
+        printf "Processing article %i" articleId
+        loadComments articleId |> saveComments
+        articleId |> saveArticle
+
+    seq {maxArticleId .. int64 0}
+        |> Seq.iter processArticle
 
     0
